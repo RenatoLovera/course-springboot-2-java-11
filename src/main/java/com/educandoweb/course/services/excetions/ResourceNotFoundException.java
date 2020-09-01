c00a3eb76cfd5a4483f3f798a3bd98c2a5b39eb8
RenatoLovera <renato.lovera@gmail.com>
2020-08-31 22:57:33 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/excetions/ResourceNotFoundException.java
+++ src/main/java/com/educandoweb/course/services/excetions/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public ResourceNotFoundException(Object id) {
-		super("Resorce not founf. Id " + id);
+		super("Resorce not found. Id " + id);
 	}
 
 }
